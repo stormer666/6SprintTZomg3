f2ad7207d56d82e50e1c29cd721872751c165171
stormer666 <stormer700@mail.ru>
2025-09-03 21:26:25 +0300

    project finished

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,64 @@
 package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func FirstHandler(res http.ResponseWriter, req *http.Request) {
+	http.ServeFile(res, req, "./index.html")
+}
+
+func SecondHandler(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(res, "incorrent method", http.StatusInternalServerError)
+		return
+	}
+
+	if err := req.ParseMultipartForm(10 << 20); err != nil {
+		http.Error(res, "ParseMultipartForm was failed", http.StatusInternalServerError)
+		return
+	}
+
+	file, fileInfo, err := req.FormFile("myFile")
+	if err != nil {
+		http.Error(res, "FormFile was failed", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	fileCht, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(res, "ReadAll was failed", http.StatusInternalServerError)
+		return
+	}
+
+	fileConv, err := service.Transform(string(fileCht))
+	conv := []byte(fileConv)
+	if err != nil {
+		http.Error(res, "Conversion was failed", http.StatusInternalServerError)
+		return
+	}
+
+	fileExt := filepath.Ext(fileInfo.Filename)
+	fileName := fmt.Sprintf("%s%s", time.Now().UTC().String(), fileExt)
+
+	if err := os.WriteFile(fileName, conv, 0755); err != nil {
+		http.Error(res, "WriteFile was failed", http.StatusInternalServerError)
+		return
+	}
+
+	res.Header().Set("Content-type", "text/plain")
+	res.WriteHeader(http.StatusOK)
+	if _, err := res.Write(conv); err != nil {
+		http.Error(res, "Write was failed", http.StatusInternalServerError)
+		return
+	}
+
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,34 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+)
+
+type homeServer struct {
+	Loger  *log.Logger
+	Server http.Server
+}
+
+func LaunchServer(logger *log.Logger) *homeServer {
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", handlers.FirstHandler)
+	router.HandleFunc("/upload", handlers.SecondHandler)
+
+	serv := &homeServer{
+		Loger: logger,
+		Server: http.Server{
+			Addr:         ":8080",
+			Handler:      router,
+			ErrorLog:     logger,
+			ReadTimeout:  5 * time.Second,
+			WriteTimeout: 10 * time.Second,
+			IdleTimeout:  15 * time.Second,
+		},
+	}
+	return serv
+}
